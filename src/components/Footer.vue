38b7c1de8e84bfed3dfa317069774906f0a4fcbb
Kim Younggeon <mail@kyg.kr>
2022-10-19 20:36:18 +0900

    feat: 출처 내용 표기

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,15 @@
 <template>
-  <footer>
-    <p class="text-xs">
+  <footer class="space-y-8 text-xs">
+    <div>
+      <h3 class="font-900">출처</h3>
+      <a target="_blank" href="https://twitter.com/pblu_nojo/status/1582658438714494978" class="underline">
+        화섬식품노조 파리바게뜨 지회 트위터
+      </a>
+
+      <a href="/injuction.txt">가처분 결과 전체 목록</a>
+    </div>
+
+    <p class="text-gray-600">
       이 사이트는 <b>파리바게뜨노동자 힘내라 공동행동</b> 및 <b>화섬식품노조 파리바게뜨지회</b>와는 관련없는 사이트임을 밝힙니다.
     </p>
   </footer>
